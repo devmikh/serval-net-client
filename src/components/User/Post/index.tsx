1966e89e956bc337440b4caa13b2ae4c2b3ed0bf
devmikh <mishacyb@gmail.com>
2023-03-20 16:36:23 -0700

    Create Post component

---
+++ src/components/User/Post/index.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import Image from 'next/image';
+
+import styles from './index.module.css';
+
+import heartIcon from '../../../../public/icons/heart.svg';
+import heartFilledIcon from '../../../../public/icons/heart-filled.svg';
+
+const Post = (props: any) => {
+
+    const [ liked, setLiked ] = useState(true);
+
+    const { avatar, fullName, username, date, text } = props;
+
+    const onLike = () => {
+        // send request to backend to like/unlike post
+        // ...
+
+        setLiked(!liked);
+    }
+    return (
+        <div className={styles.postContainer}>
+            <div className={styles.header}>
+                <Image className={styles.avatar} src={avatar} alt='avatar' width={60} unoptimized={true} />
+                <div className={styles.nameContainer}>
+                    <span className={styles.fullName}>{fullName}</span>
+                    <span className={styles.username}>@{username}</span>
+                </div>
+                <span className={styles.date}>{date}</span>
+            </div>
+            <p className={styles.text}>{text}</p>
+            <div className={styles.actionsContainer}>
+                <Image
+                    className={styles.heart}
+                    src={ liked ? heartFilledIcon : heartIcon }
+                    alt='like'
+                    width={24}
+                    onClick={() => onLike()}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Post;
--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -1,7 +1,17 @@
 import UserProfile from "@/components/UserProfile/UserProfile";
+import avatar from "../../../public/images/avatar.jpg";
+import Post from "@/components/User/Post";
 
 const UserProfilePage = () => {
-    return <UserProfile />
+    return (
+        <Post
+            avatar={avatar}
+            fullName={'Mikhail Serebriakov'}
+            username={'misha'}
+            date={'1 day ago'}
+            text={'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu'}
+        />
+    )
 }
 
 export default UserProfilePage;
