a318a87df05d8868107e6a2b7689f3a512402536
devmikh <mishacyb@gmail.com>
2023-03-30 11:37:56 -0700

    Implement sending request to create a post to the backend

--- src/components/core/PostInput/index.tsx
+++ src/components/core/PostInput/index.tsx
@@ -1,16 +1,35 @@
-import Button from '../Button';
+import axios from 'axios';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { fetchPostsByUserId } from '@/store/features/postsSlice';
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+
 import styles from './index.module.css';
 
 const PostInput = () => {
 
-    const submitPost = (event: any) => {
+    const dispatch = useAppDispatch();
+    const user = useSelector((state: any) => state.currentUser.data);
+
+    const [postText, setPostText] = useState('');
+
+    const handleChange = (event: any) => {
+        setPostText(() => event.target.value);
+    }
+
+    const submitPost = async (event: any) => {
         event.preventDefault();
         
+        const response = await axios.post('http://localhost:3030/api/createPost', { text: postText}, {withCredentials: true});
+        if (response.status === 200) {
+            dispatch(fetchPostsByUserId(user.id));
+        }
     }
 
     return (
         <form className={styles.container} onSubmit={submitPost}>
-            <textarea className={styles.textarea} placeholder="What's on your mind?" />
+            <textarea name="postText" className={styles.textarea} placeholder="What's on your mind?" maxLength={100} onChange={handleChange} />
             <button type='submit' className={styles.button}>Post</button>
         </form>
     )
