5182a768cd76bc065c80990440122b6a2b9e2c37
devmikh <mishacyb@gmail.com>
2023-04-12 16:06:51 -0700

    Cleanup and refactor

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,12 +1,21 @@
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import styles from './index.module.css';
 import logo from '../../../public/icons/serval-logo.svg';
 
 const NotFound = () => {
+
+    const router = useRouter();
+
+    const onClick = () => {
+        router.push('/');
+    }
+
     return (
         <div className={styles.container}>
             <Image src={logo} alt='logo' width={64} className={styles.logo}/>
             <h1 className={styles.message}>404 | Page not found</h1>
+            <button className={styles.homeLink} onClick={onClick}>Home</button>
         </div>
     )
 }
