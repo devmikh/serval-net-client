943cfe95d94d91dada3bd34b52bb286575296ef4
devmikh <mishacyb@gmail.com>
2023-04-14 17:46:47 -0700

    Add scroll to top button

---
+++ src/components/core/ScrollToTopButton/index.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import styles from './index.module.css';
+import arrowIcon from '../../../../public/icons/angles-up-solid.svg';
+
+const ScrollToTopButton = () => {
+
+    const [visible, setVisible] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 300) {
+            setVisible(true);
+        } else {
+            setVisible(false);
+        }
+    }
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth'});
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, []);
+
+    return (
+        <button className={`${styles.scrollToTopButton} ${visible ? styles.visible : null}`} onClick={scrollToTop}>
+            <Image src={arrowIcon} className={styles.icon} alt='scroll up' width={32} height={32}/>
+        </button>
+    )
+}
+
+export default ScrollToTopButton;
